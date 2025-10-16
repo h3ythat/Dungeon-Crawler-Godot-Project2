24c96576783afeb9401b9f4219a0e718f3348538
h3ythat <Ahyan.Dewan@endeavour.sa.edu.au>
2025-10-16 22:37:37 +1030

    major update to slash graphics

--- particles.gd
+++ particles.gd
@@ -1,25 +1,47 @@
 extends Node2D
 @onready var Line1: Line2D = $Line1
 @onready var Line2: Line2D = $Line2
+var slashd: bool = false
+var attack_active = false
+var prev_rotat
 
+signal weapon_charged
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"../Player/CharacterBody2D/Node2D".line1pos.connect(self.line1maker)
 	$"../Player/CharacterBody2D/Node2D".line2pos.connect(self.line2maker)
-
+	$"../Player/CharacterBody2D/Node2D".SLASH.connect(self.slash)
+	$"../Player/CharacterBody2D/Node2D".done_slash.connect(self.dslash)
+	$"../Player/CharacterBody2D/Node2D".attack_not_detected.connect(self.no_attack_input)
+	$"../Player/CharacterBody2D/Node2D".attack_detected.connect(self.attack_input)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func line1maker(lpos: Vector2):
-	print(Line1.points.size())
-	if(Line1.points.size() < 60):
-		Line1.add_point(lpos)
+	#if(attack_active == false):
+		#Line1.clear_points()
+	if(Line1.points.size() > 20):
+		Line1.remove_point(0)
+		Line1.remove_point(0)
+	if(!slashd):
+		Line1.width = 7+(Line1.width*delta*2)
 		
-	else:
+		Line1.set_gradient(load("res://gradient_normal.tres"))
+		#var cool: Color = Color(1.0, 0.2, 0.153, 0.741) 
+#		Line1.gradient.set_color(1, cool)
+		
+func line1maker(lpos: Vector2, rotat):
+	print(Line1.points.size())
+	if(prev_rotat == rotat):
 		Line1.remove_point(0)
-		Line1.remove_point(0)
+	if(Line1.points.size() < 60 && prev_rotat != rotat):
 		Line1.add_point(lpos)
+		prev_rotat = rotat
+	else:
+		if(prev_rotat != rotat):
+			prev_rotat = rotat
+			Line1.remove_point(0)
+			Line1.remove_point(0)
+			Line1.add_point(lpos)
+	
 
 func line2maker(lpos: Vector2):
 	print(Line2.points.size())
@@ -31,5 +53,22 @@ func line2maker(lpos: Vector2):
 		Line2.remove_point(0)
 		Line2.add_point(lpos)		
 		
+func slash():
+	weapon_charged.emit()
+	slashd = true
+	Line1.set_gradient(load("res://red_grad.tres"))
+func dslash():
+	if(slashd == true):
+		await get_tree().create_timer(.5).timeout
+		slashd = false
+		attack_active = false
+	slashd = false
+	attack_active = false
+func no_attack_input():
+	#attack_active = false
+	Line1.remove_point(0)
+	
+func attack_input():
+	attack_active = true
 	
 	
--- sword_script.gd
+++ sword_script.gd
@@ -3,15 +3,25 @@ var rotation_speed = 0.05
 var rotat
 var new_rotat
 var current_rotat
+@export var shad: ShaderMaterial
+var charged_weapon = false
+
 signal line1pos
 signal line2pos
+signal SLASH
+signal done_slash
+signal attack_not_detected
+signal attack_detected
+
 @onready var point_1: Node2D = $"Particle point 1"
 @onready var point_2: Node2D = $"Particle point 2"
 @onready var Line1: Line2D = $"../Line1"
 @onready var Line2: Line2D = $"../Line2"
+@onready var particle_handler = $"../../../particles"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	particle_handler.weapon_charged.connect(self.sword_charged_shader)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,16 +31,19 @@ func _process(delta):
 		new_rotat = rotat - 3.14159*3
 	if(Input.is_action_pressed("rotate_sword_left")):
 		current_rotat = self.get_rotation()
-		if(rotation_speed < 0.30):
-			rotation_speed = rotation_speed + (0.36*delta)
+		if(rotation_speed < 0.35):
+			rotation_speed = rotation_speed + (0.65*delta)
 		else:
 			print("hungryz")
+			SLASH.emit()
 			print(rotat)
 			print(new_rotat)
 		if(current_rotat > new_rotat):
 			rotate(-1*rotation_speed)
 		if(current_rotat < new_rotat):
 			rotation_speed = 0.05
+			done_slash.emit()
+			charged_weapon = false
 	
 	if(Input.is_action_just_pressed("rotate_sword_right")):
 		rotat = self.get_rotation()
@@ -38,25 +51,54 @@ func _process(delta):
 	if(Input.is_action_pressed("rotate_sword_right")):
 		current_rotat = self.get_rotation()
 		if(rotation_speed < 0.30):
-			rotation_speed = rotation_speed + (0.35*delta)
+			rotation_speed = rotation_speed + (0.65*delta)
 			print(rotation_speed)
 		else:
 			print("hungryz")
+			SLASH.emit()
 			print(rotat)
 			print(new_rotat)
 		if(current_rotat < new_rotat):
 			rotate(rotation_speed)
 		if(current_rotat >= new_rotat):
 			rotation_speed = 0.05
+			done_slash.emit()
+			charged_weapon = false
 	if(Input.is_action_just_released("rotate_sword_left") || Input.is_action_just_released("rotate_sword_right")):
 		rotation_speed = 0.05
-		
+		done_slash.emit()
+		charged_weapon = false
+	if(Input.is_action_just_pressed("rotate_sword_left") || Input.is_action_just_pressed("rotate_sword_right")):
+		attack_detected.emit()
 	if(Input.is_action_pressed("rotate_sword_left") || Input.is_action_pressed("rotate_sword_right")):
-		line1pos.emit(point_1.global_position)
-		line2pos.emit(point_2.global_position)
+		line1pos.emit(point_1.global_position, current_rotat)
+		line2pos.emit(point_2.global_position, current_rotat)
+	if(!Input.is_action_pressed("rotate_sword_left") && !Input.is_action_pressed("rotate_sword_right")):
+		attack_not_detected.emit()
 		#print(Line1.points.size())
 		#if(Line1.points.size() < 30):
 			#line1pos.emit(point_1.global_position)
 			#Line1.add_point(point_1.global_position)
 		#else:
 			#Line1.points.remove_at(0)
+	
+	if(charged_weapon == true && shad != null):
+		shad.set_shader_parameter("blend_strength", shad.get_shader_parameter("blend_strength")+0.08)
+		print("DRIVING IN MY CAR")
+	else:
+		if(shad != null):
+			while($Sword.material.get_shader_parameter("blend_strength") > 0.1):
+				await get_tree().create_timer(.1).timeout
+				if($Sword.material.get_shader_parameter("blend_strength") >= 0):
+					shad.set_shader_parameter("blend_strength", shad.get_shader_parameter("blend_strength")-0.01)
+				else:
+					shad.set_shader_parameter("blend_strength", -0.1)
+					await get_tree().create_timer(.1).timeout
+					shad.set_shader_parameter("blend_strength", 0)
+func sword_charged_shader():
+	charged_weapon = true
+	shad = $Sword.material
+func sword_decharged():
+	pass
+	
+	
